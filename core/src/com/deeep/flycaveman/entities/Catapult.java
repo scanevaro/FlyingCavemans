81ee8ea0db688f45584ff80a87638e705878d1ad
Sebastián <scanevaro@hotmail.com>
2015-02-19 18:07:13 -0300

    :trihard: push

--- core/src/com/deeep/flycaveman/entities/Catapult.java
+++ core/src/com/deeep/flycaveman/entities/Catapult.java
@@ -107,7 +107,7 @@ public class Catapult implements Entity {
         armJointDef.motorSpeed = -10/*-1260*/;
         armJointDef.lowerAngle = (float) Math.toRadians(9);
         armJointDef.upperAngle = (float) Math.toRadians(75);
-        armJointDef.maxMotorTorque = 5000;
+        armJointDef.maxMotorTorque = 4000;
 
         armJoint = (RevoluteJoint) world.createJoint(armJointDef);
     }
