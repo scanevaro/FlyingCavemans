6e3708450ba225cf9b17694c7dead0baa7994bc0
Kruhlmann <andreaskruhlmann@gmail.com>
2015-02-26 00:15:07 +0100

    Sound Manager docs

--- core/src/com/deeep/flycaveman/classes/SoundManager.java
+++ core/src/com/deeep/flycaveman/classes/SoundManager.java
@@ -11,14 +11,27 @@ public class SoundManager {
     public SoundManager() {
     }
 
+    /**
+     * Plays a sound using the LIBGDX sound library
+     * @param sound Sound to be played
+     */
     public void playSound(Sound sound){
         sound.play();
     }
 
+    /**
+     * Loops a sound using the LIBGDX sound library
+     * @param sound Sound to be looped
+     */
     public void loopSound(Sound sound){
         sound.loop();
     }
 
+    /**
+     * Plays or loops a music file using the LIBGDX music library
+     * @param music Music to be played/looped
+     * @param loop Decides to loop or not
+     */
     public void playMusic(Music music, boolean loop){
         if(loop){
             if(!music.isLooping()) music.setLooping(true);
@@ -27,19 +40,35 @@ public class SoundManager {
         if(!music.isPlaying()) music.play();
     }
 
+    /**
+     * Get music from the library with string as identifier
+     * @param music identifier string
+     * @return music object file
+     */
     public Music getMusic(String music){
         return SoundLibrary.getInstance().getMusicFromString(music);
     }
 
+    /**
+     * Stops a sound from playing
+     * @param sound object to stop
+     */
     public void stopSound(Sound sound){
         sound.stop();
     }
 
+    /**
+     * Stops a music object from playing
+     * @param music object to stop
+     */
     public void stopMusic(Music music){
         music.stop();
     }
 }
 
+/**
+ * Library class for handling the retrieval of sound files from the assets manager using strings as identifiers
+ */
 class SoundLibrary {
     public static SoundLibrary getInstance(){
         return new SoundLibrary();
