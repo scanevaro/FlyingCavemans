010ad2941153ac548238754c6baa7af60340dcdd
Sebastián <scanevaro@hotmail.com>
2015-04-11 18:39:24 -0300

    :trihard:  powerups feedback UI WIP

--- core/src/com/deeep/flycaveman/widgets/PickupsWidget.java
+++ core/src/com/deeep/flycaveman/widgets/PickupsWidget.java
@@ -18,26 +18,26 @@ public class PickupsWidget extends Actor {
     public PickupsWidget() {
         stamina = new Image(Assets.staminaText);
         stamina.setSize(stamina.getPrefWidth(), stamina.getPrefHeight());
-        stamina.setPosition(-stamina.getWidth(), Core.VIRTUAL_HEIGHT / 2 - stamina.getHeight());
+        stamina.setPosition(-stamina.getWidth() - 20, Core.VIRTUAL_HEIGHT / 2 - stamina.getHeight());
         stamina.setRotation(15);
         percentage = new Image(Assets.percentage);
         percentage.setSize(percentage.getPrefWidth(), percentage.getPrefHeight());
         percentage.setPosition(-percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - percentage.getHeight() / 2);
-        plus = new Image(Assets.plus);
-        plus.setSize(plus.getPrefWidth(), plus.getPrefHeight());
-        plus.setPosition(-plus.getWidth(), Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2);
-        minus = new Image(Assets.minus);
-        minus.setSize(minus.getPrefWidth(), minus.getPrefHeight());
-        minus.setPosition(-minus.getWidth(), Core.VIRTUAL_HEIGHT / 2 - minus.getHeight() / 2);
         infinite = new Image(Assets.infinite);
         infinite.setSize(infinite.getPrefWidth(), infinite.getPrefHeight());
-        infinite.setPosition(-infinite.getWidth(), Core.VIRTUAL_HEIGHT / 2 - infinite.getHeight() / 2);
+        infinite.setPosition(-infinite.getWidth() - 45, Core.VIRTUAL_HEIGHT / 2 - infinite.getHeight() / 2);
         one = new Image(Assets.one);
         one.setSize(one.getPrefWidth(), one.getPrefHeight());
-        one.setPosition(-one.getWidth(), Core.VIRTUAL_HEIGHT / 2 - one.getHeight() / 2);
+        one.setPosition(-one.getWidth() - percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - one.getHeight() / 2);
         five = new Image(Assets.five);
         five.setSize(five.getPrefWidth(), five.getPrefHeight());
-        five.setPosition(-five.getWidth(), Core.VIRTUAL_HEIGHT / 2 - five.getHeight() / 2);
+        five.setPosition(-five.getWidth() - percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - five.getHeight() / 2);
+        plus = new Image(Assets.plus);
+        plus.setSize(plus.getPrefWidth(), plus.getPrefHeight());
+        plus.setPosition(-plus.getWidth() - one.getWidth() - percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2);
+        minus = new Image(Assets.minus);
+        minus.setSize(minus.getPrefWidth(), minus.getPrefHeight());
+        minus.setPosition(-minus.getWidth() - five.getWidth() - percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - minus.getHeight() / 2);
     }
 
     @Override
@@ -110,15 +110,17 @@ public class PickupsWidget extends Actor {
                     Actions.delay(0.35f),
                     Actions.moveTo(-percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - percentage.getHeight() / 2, 0.3f)));
             five.addAction(new SequenceAction(
-                    Actions.moveTo(35, Core.VIRTUAL_HEIGHT / 2 - stamina.getHeight(), 0.3f),
+                    Actions.moveTo(64, Core.VIRTUAL_HEIGHT / 2 - five.getHeight() / 2, 0.3f),
                     Actions.scaleBy(0.25f, 0.25f, 0.2f), Actions.scaleBy(-0.25f, -0.25f, 0.2f),
                     Actions.delay(0.35f),
-                    Actions.moveTo(-stamina.getWidth(), Core.VIRTUAL_HEIGHT / 2 - stamina.getHeight(), 0.3f)));
+                    Actions.moveTo(-five.getWidth() - percentage.getWidth(),
+                            Core.VIRTUAL_HEIGHT / 2 - five.getHeight() / 2, 0.3f)));
             plus.addAction(new SequenceAction(
                     Actions.moveTo(0, Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2, 0.3f),
                     Actions.scaleBy(0.25f, 0.25f, 0.2f), Actions.scaleBy(-0.25f, -0.25f, 0.2f),
                     Actions.delay(0.35f),
-                    Actions.moveTo(-plus.getWidth(), Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2, 0.3f)));
+                    Actions.moveTo(-plus.getWidth() - five.getWidth() - percentage.getWidth(),
+                            Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2, 0.3f)));
         } else if (type == PowerUp.Type.MEAT) {
             stamina.addAction(new SequenceAction(
                     Actions.moveTo(20, Core.VIRTUAL_HEIGHT / 2 - (stamina.getHeight() * 2), 0.3f),
@@ -131,15 +133,16 @@ public class PickupsWidget extends Actor {
                     Actions.delay(0.35f),
                     Actions.moveTo(-percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - percentage.getHeight() / 2, 0.3f)));
             one.addAction(new SequenceAction(
-                    Actions.moveTo(35, Core.VIRTUAL_HEIGHT / 2 - one.getHeight() / 2, 0.3f),
+                    Actions.moveTo(64, Core.VIRTUAL_HEIGHT / 2 - one.getHeight() / 2, 0.3f),
                     Actions.scaleBy(0.25f, 0.25f, 0.2f), Actions.scaleBy(-0.25f, -0.25f, 0.2f),
                     Actions.delay(0.25f),
-                    Actions.moveTo(-one.getWidth(), Core.VIRTUAL_HEIGHT / 2 - one.getHeight() / 2, 0.3f)));
+                    Actions.moveTo(-one.getWidth() - percentage.getWidth(), Core.VIRTUAL_HEIGHT / 2 - one.getHeight() / 2, 0.3f)));
             plus.addAction(new SequenceAction(
                     Actions.moveTo(0, Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2, 0.3f),
                     Actions.scaleBy(0.25f, 0.25f, 0.2f), Actions.scaleBy(-0.25f, -0.25f, 0.2f),
                     Actions.delay(0.35f),
-                    Actions.moveTo(-plus.getWidth(), Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2, 0.3f)));
+                    Actions.moveTo(-plus.getWidth() - one.getWidth() - percentage.getWidth(),
+                            Core.VIRTUAL_HEIGHT / 2 - plus.getHeight() / 2, 0.3f)));
         }
     }
 }
