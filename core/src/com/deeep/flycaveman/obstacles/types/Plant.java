0e756b73aa6ef1a6aca564ce08fcb1623b790381
Sebastián <scanevaro@hotmail.com>
2015-08-02 16:20:01 -0300

    :noscope: new art and fixes, need to fix new art :noscope:

--- core/src/com/deeep/flycaveman/obstacles/types/Plant.java
+++ core/src/com/deeep/flycaveman/obstacles/types/Plant.java
@@ -1,6 +1,6 @@
 package com.deeep.flycaveman.obstacles.types;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.deeep.flycaveman.obstacles.ObstacleBase;
 import com.deeep.flycaveman.world.World;
@@ -9,7 +9,7 @@ import com.deeep.flycaveman.world.World;
  * Created by Elmar on 6-6-2015.
  */
 public class Plant extends ObstacleBase {
-    public Plant(TextureRegion textureRegion, float spriteSizeX, float spriteSizeY, float sizeX, float sizeY, World world, Body body) {
-        super(textureRegion, spriteSizeX, spriteSizeY, sizeX, sizeY, world, body);
+    public Plant(Animation animation, float spriteSizeX, float spriteSizeY, float sizeX, float sizeY, World world, Body body) {
+        super(animation, spriteSizeX, spriteSizeY, sizeX, sizeY, world, body);
     }
 }
